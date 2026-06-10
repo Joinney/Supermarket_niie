ead82c874a3bcc59b81b7366e007fc83f8f6117a
Vo Duy Toan <voduyduydlk@gmail.com>
2026-06-11 02:12:26 +0700

    fix deylop

--- backend-node/services/product-service/configs/database.js
+++ backend-node/services/product-service/configs/database.js
@@ -10,10 +10,10 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 
   // 2. Thông số dự phòng (Nếu DATABASE_URL trống)
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'host.docker.internal', // Đã sửa đuôi chuẩn
+  user: process.env.DB_USER || 'postgres.aiesurvlmtrrgdiwxtma',
+  host: process.env.DB_HOST || 'aws-1-ap-southeast-1.pooler.supabase.com', // Đã sửa đuôi chuẩn
   database: process.env.DB_NAME || 'demi_product_db',
-  password: process.env.DB_PASSWORD || '123456',
+  password: process.env.DB_PASSWORD || 'demimart@2026',
   port: parseInt(process.env.DB_PORT || '5432'),
 
   // 3. SSL là bắt buộc khi kết nối tới Render DB từ bên ngoài hoặc chạy production
