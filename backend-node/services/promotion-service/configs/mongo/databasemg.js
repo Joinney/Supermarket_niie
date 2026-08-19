27bdbbaa76e5989b4a5ab73a9d8be9da06b2d9ec
Joinney <voduyduydlk@gmail.com>
2026-08-19 17:24:47 +0700

    fix bảo mật key

--- backend-node/services/promotion-service/configs/mongo/databasemg.js
+++ backend-node/services/promotion-service/configs/mongo/databasemg.js
@@ -11,8 +11,12 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const MONGO_URI = process.env.MONGO_URI || "mongodb://voduyduydlk_db_user:DemiMart2026@ac-kf1htmb-shard-00-00.kozikg3.mongodb.net:27017,ac-kf1htmb-shard-00-01.kozikg3.mongodb.net:27017,ac-kf1htmb-shard-00-02.kozikg3.mongodb.net:27017/poster-service?ssl=true&replicaSet=atlas-gwlp8o-shard-0&authSource=admin&appName=DemiMartMGDB";
-    
+    const MONGO_URI = process.env.MONGO_URI;
+
+    if (!MONGO_URI) {
+      throw new Error("Biến môi trường MONGO_URI chưa được định nghĩa trong file .env!");
+    }
+
     await mongoose.connect(MONGO_URI);
     console.log("🍃 [POSTER-SERVICE] Kết nối thành công cơ sở dữ liệu MongoDB Atlas (Trụ sở)!");
   } catch (error) {
